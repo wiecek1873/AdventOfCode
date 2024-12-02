c1a2f2e20e538540b8bd0e0ba7a6122a68c5644d
Michał Więcek <wiecek1873@gmail.com>
2024-12-02 20:36:03 +0100

    Init + day 1

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode2024.Solvers;
+
+Console.WriteLine("Hello, World!");
+
+var day01Solver = new Day01Solver();
+await day01Solver.SolveAsync();
---
+++ AdventOfCode2024/Solvers/Day01Solver.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2024.Solvers;
+
+public class Day01Solver
+{
+    public static StreamReader GetInputStream()
+    {
+        return new StreamReader("Inputs/01.txt");
+    }
+
+    public async Task SolveAsync()
+    {
+        var inputStream = GetInputStream();
+
+        var line = await inputStream.ReadLineAsync();
+
+        var sum = 0;
+
+        while (line != null)
+        {
+            var nums = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            sum += int.Parse(nums[0]) - int.Parse(nums[1]);
+            line = await inputStream.ReadLineAsync();
+        }
+
+        Console.WriteLine($"Day 1, Part 1 answer is: {sum}");
+    }
+
+}
