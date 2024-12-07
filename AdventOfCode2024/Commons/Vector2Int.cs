8c32798f5347fa5b469556f8a9dc81ed69cc9a7e
Michał Więcek <wiecek1873@gmail.com>
2024-12-07 20:49:32 +0100

    Added Vector2Int

---
+++ AdventOfCode2024/Commons/Vector2Int.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2024.Commons;
+public struct Vector2Int
+{
+    public int X;
+    public int Y;
+
+    public Vector2Int(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Vector2Int vector&&
+               X==vector.X&&
+               Y==vector.Y;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+
+    public static bool operator ==(Vector2Int left, Vector2Int right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Vector2Int left, Vector2Int right)
+    {
+        return !(left==right);
+    }
+
+    public override string ToString()
+    {
+        return $"({X},{Y})";
+    }
+}
