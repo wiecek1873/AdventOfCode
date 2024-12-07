4a579709820c04092f98b7890ae68a3e23ea384a
Michał Więcek <wiecek1873@gmail.com>
2024-12-07 18:11:23 +0100

    Sketch for Day 6 + input

---
+++ AdventOfCode2024.Tests/Solvers/Day06Tests.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode2024.Solvers;
+
+namespace AdventOfCode2024.Tests.Solvers;
+public class Day06Tests
+{
+    private readonly Day06 day06 = new Day06();
+
+
+}
---
+++ AdventOfCode2024/Solvers/Day06.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode2024.Solvers;
+public class Day06 : Solver
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public override Task SolveAsync()
+    {
+        var input = GetInputStream("06");
+
+        throw new NotImplementedException();
+    }
+
+    public static void FindGuardPosition(char[][] map, out int guardI, out int guardJ)
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            for (int j = 0; j < map.Length; j++)
+            {
+                if (map[i][j] == '^')
+                {
+                    guardI = i;
+                    guardJ = j;
+                    return;
+                }
+            }
+        }
+
+        throw new InvalidOperationException("Map does not contain guard");
+    }
+
+    public static void MoveGuard(char[][] map, ref int guardI, out int guardJ)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static char[][] FillWithGuardPath(char[][] map)
+    {
+        FindGuardPosition(map, out int guardI, out int guardJ);
+
+        while (guardI != -1 && guardJ != -1)
+        {
+            MoveGuard(map, ref guardI, out guardJ);
+        }
+    }
+}
